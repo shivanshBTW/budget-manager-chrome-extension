fe7f5ce46f44d116f67a85b8d41e445965a79d4b
Shivansh Tyagi <stshivansh9@gmail.com>
2019-08-06 16:55:04 +0530

    Revert "completed and cleaned up the code"
    This reverts commit 52836285d7bb988aa46fa3e59b0180808b615081.

--- javascript/background.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.sendRequestType == 281) {
-        chrome.tabs.create({url: "https://onlineservices.tin.egov-nsdl.com/etaxnew/tdsnontds.jsp"}, (tab) => {
-            chrome.tabs.sendMessage(tab.id, {sendRequestType: 281});
-            chrome.tabs.executeScript(tab.id, {code: "let callItScript = document.createElement('script');callItScript.innerHTML = 'sendRequest(281);';document.getElementById('footerDiv').appendChild(callItScript);"})
-        });
-    }
-});
